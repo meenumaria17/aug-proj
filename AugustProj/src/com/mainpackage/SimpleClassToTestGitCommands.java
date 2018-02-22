1be99814aefd0d9d776627827f8939b7cd5bd0c8
meenu abraham <meenu abraham>
2018-02-23 00:26:04 +0530

    Added new class to test git

---
+++ AugustProj/src/com/mainpackage/SimpleClassToTestGitCommands.java
@@ -0,0 +1,10 @@
+package com.mainpackage;
+
+public class SimpleClassToTestGitCommands {
+	
+	public void displayToScreen()
+	{
+		System.out.println("Hi, im just a dummy class to test git commands");
+	}
+
+}
