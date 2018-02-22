1143063a97c0225ffc4829d3b005cbe4eb9c99cc
Meenu <meenuabr@live.com>
2018-02-23 00:49:49 +0530

    making changes in the git dummy class

--- AugustProj/src/com/mainpackage/SimpleClassToTestGitCommands.java
+++ AugustProj/src/com/mainpackage/SimpleClassToTestGitCommands.java
@@ -5,6 +5,7 @@ public class SimpleClassToTestGitCommands {
 	public void displayToScreen()
 	{
 		System.out.println("Hi, im just a dummy class to test git commands");
+		System.out.println("Adding another dummy line");
 	}
 
 }
