367902506fcf53181c194f0c975641b31f2d2875
AnonymousMeenu <meenu.mabraham@gmail.com>
2018-02-23 01:01:31 +0530

    Adding dummy variable too

--- AugustProj/src/com/mainpackage/SimpleClassToTestGitCommands.java
+++ AugustProj/src/com/mainpackage/SimpleClassToTestGitCommands.java
@@ -2,6 +2,8 @@ package com.mainpackage;
 
 public class SimpleClassToTestGitCommands {
 	
+	private String dummy_variable = "please work!";
+	
 	public void displayToScreen()
 	{
 		System.out.println("Hi, im just a dummy class to test git commands");
